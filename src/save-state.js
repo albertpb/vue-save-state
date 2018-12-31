8f7c14d621087936281d69186cba1ab738ea375d
Albert <palumbo.alb@gmail.com>
2018-12-31 10:21:35 -0300

    Disable load on create

--- src/save-state.js
+++ src/save-state.js
@@ -1,32 +1,32 @@
-import pickBy from 'lodash/pickBy';
-import forEach from 'lodash/forEach';
-import { saveState, getSavedState, clearSavedState } from './local-storage';
+import pickBy from "lodash/pickBy";
+import forEach from "lodash/forEach";
+import { saveState, getSavedState, clearSavedState } from "./local-storage";
 
 export default {
     watch: {
-        '$data': {
+        $data: {
             handler() {
                 this.saveState();
             },
-            deep: true,
-        },
+            deep: true
+        }
     },
 
     created() {
-        this.loadState();
+        // this.loadState();
     },
 
     methods: {
         loadState() {
-
-            const savedState = getSavedState(this.getSaveStateConfig().cacheKey);
+            const savedState = getSavedState(
+                this.getSaveStateConfig().cacheKey
+            );
 
             if (!savedState) {
                 return;
             }
 
             forEach(savedState, (value, key) => {
-
                 if (this.attributeIsManagedBySaveState(key)) {
                     if (this.getSaveStateConfig().onLoad) {
                         value = this.getSaveStateConfig().onLoad(key, value);
@@ -46,21 +46,27 @@ export default {
         },
 
         attributeIsManagedBySaveState(attribute) {
-            if (this.getSaveStateConfig().ignoreProperties &&
-                this.getSaveStateConfig().ignoreProperties.indexOf(attribute) !== -1) {
-
+            if (
+                this.getSaveStateConfig().ignoreProperties &&
+                this.getSaveStateConfig().ignoreProperties.indexOf(
+                    attribute
+                ) !== -1
+            ) {
                 return false;
             }
 
-            if (! this.getSaveStateConfig().saveProperties) {
+            if (!this.getSaveStateConfig().saveProperties) {
                 return true;
             }
 
-            return this.getSaveStateConfig().saveProperties.indexOf(attribute) !== -1;
+            return (
+                this.getSaveStateConfig().saveProperties.indexOf(attribute) !==
+                -1
+            );
         },
 
         clearSavedState() {
             clearSavedState(this.getSaveStateConfig().cacheKey);
-        },
-    },
+        }
+    }
 };
